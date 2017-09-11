ea0755066cf1c62e5f2de082da8310b551cc44ad
Tim Buckland-Kerr <timbk86@gmail.com>
2017-09-12 05:36:16 +1000

    finish sec 24 lec 316 (maybe)

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -2,10 +2,9 @@ import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
-import { ShoppingListService } from './shopping-list.service';
 import { Store } from '@ngrx/store';
-import * as fromShoppingList from './store/shopping-list.reducers'
-
+import * as ShoppingListActions from '../shopping-list/store/shopping-list.actions'
+import * as fromApp from '../store/app.reducers'
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -14,13 +13,13 @@ import * as fromShoppingList from './store/shopping-list.reducers'
 export class ShoppingListComponent implements OnInit {
   shoppingListState: Observable<{ingredients: Ingredient[]}>;
 
-  constructor(private slService: ShoppingListService, private store: Store<fromShoppingList.AppState>) { }
+  constructor(private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
     this.shoppingListState = this.store.select('shoppingList');
   }
 
   onEditItem(index: number) {
-    this.slService.startedEditing.next(index);
+    this.store.dispatch(new ShoppingListActions.StartEdit(index));
   }
 }
--- src/app/shopping-list/store/shopping-list.reducers.ts
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -1,17 +1,13 @@
 import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from './shopping-list.actions'
 
-export interface AppState {
-  shoppingList: State
-}
-
 export interface State {
   ingredients: Ingredient[],
   editedIngredient: Ingredient,
   editedIngredientIndex: number
 }
 
-const initialState = {
+const initialState: State = {
   ingredients: [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10),
@@ -34,23 +30,40 @@ export function shoppingListReducer(state = initialState, action: ShoppingListAc
         ingredients: [...state.ingredients, ...action.payload]
       }
     case ShoppingListActions.UPDATE_INGREDIENT:
-    const ingredient = state.ingredients[action.payload.index];
+    const ingredient = state.ingredients[state.editedIngredientIndex];
     const updatedIngredient = {
       ...ingredient,
       ...action.payload.ingredient
     };
     const ingredients = [...state.ingredients];
-    ingredients[action.payload.index] = updatedIngredient;
+    ingredients[state.editedIngredientIndex] = updatedIngredient;
       return {
         ...state,
-        ingredients: ingredients
+        ingredients: ingredients,
+        editedIngredient: null,
+        editedIngredientIndex: -1
       }
     case ShoppingListActions.DELETE_INGREDIENT:
       const oldIngredients = [...state.ingredients];
-      oldIngredients.splice(action.payload, 1);
+      oldIngredients.splice(state.editedIngredientIndex, 1);
       return {
         ...state,
-        ingredients: oldIngredients
+        ingredients: oldIngredients,
+        editedIngredient: null,
+        editedIngredientIndex: -1
+      }
+    case ShoppingListActions.START_EDIT:
+      const editedIngredient = {...state.ingredients[action.payload]};
+      return{
+        ...state,
+        editedIngredient: editedIngredient,
+        editedIngredientIndex: action.payload
+      }
+    case ShoppingListActions.STOP_EDIT:
+      return {
+        ...state,
+        editedIngredient: null,
+        editedIngredientIndex: -1
       }
     default:
       return state
